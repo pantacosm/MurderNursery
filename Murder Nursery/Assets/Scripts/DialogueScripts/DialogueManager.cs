29a2b46c86fb3a616cf4d6e3f98e447084047d10
AidenD1799 <65459473+AidenD1799@users.noreply.github.com>
2022-11-08 19:53:49 +0000

    Code Commenting for Dialogue Scripts

--- Murder Nursery/Assets/Scripts/DialogueScripts/DialogueManager.cs
+++ Murder Nursery/Assets/Scripts/DialogueScripts/DialogueManager.cs
@@ -34,22 +34,22 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(inConvo)
+        if(inConvo) //Checks if the player is still in a conversation
         {
             ContinueConversation();
         }
     }
 
-    public void StartConversation(DialogueNode startNode, GameObject npc)
+    public void StartConversation(DialogueNode startNode, GameObject npc) //Is called when a player interacts with an npc and begins a conversation
     {
         activeNPC = npc;
-        LoadNodeInfo(startNode);
+        LoadNodeInfo(startNode); //Loads the intro node info into the dialogue UI
         dialogueZone.SetActive(true);
         player.GetComponent<CharacterController>().enabled = false;
-        inConvo = true;
+        inConvo = true; //Indicates that the player is in a conversations
     }
 
-    public void ExitConversation()
+    public void ExitConversation() //Is called when the player is finished speaking to an npc
     {
         activeNPC = null;
         dialogueZone.SetActive(false);
@@ -57,71 +57,71 @@ public class DialogueManager : MonoBehaviour
         inConvo = false;
     }
 
-    public void LoadNodeInfo(DialogueNode newNode)
+    public void LoadNodeInfo(DialogueNode newNode) //Is called when we want to load a new npc statement and a new set of player responses
     {
         if(activeNode != null)
         {
-            activeNode.nodeActive = false;
+            activeNode.nodeActive = false; //Signals that the previous node is no longer active
         }
-        activeNode = newNode;
+        activeNode = newNode; //Signals that the new node is active
         newNode.nodeActive = true;
         npcStatement.GetComponent<TextMeshProUGUI>().text = newNode.speech;
 
-        if(activeNode.responses.Length == 0)
+        if(activeNode.responses.Length == 0) //Checks if the player has any possible responses
         {
-            playerFirstResponse.GetComponent<TextMeshProUGUI>().text = "Press Escape To Leave Conversation";
+            playerFirstResponse.GetComponent<TextMeshProUGUI>().text = "Press Escape To Leave Conversation"; //Tells the player it's time to leave the conversation
             playerSecondResponseBox.SetActive(false);
             playerThirdResponseBox.SetActive(false);
         }
-        if (activeNode.responses.Length == 1)
+        if (activeNode.responses.Length == 1) //Checks if the player has only one response
         {
-            playerSecondResponseBox.SetActive(false);
+            playerSecondResponseBox.SetActive(false); //Disables the unused text boxes
             playerThirdResponseBox.SetActive(false);
         }
-        if (activeNode.responses.Length == 2)
+        if (activeNode.responses.Length == 2) //Checks if the player has only two responses
         {
-            playerThirdResponseBox.SetActive(false);
+            playerThirdResponseBox.SetActive(false); //Disabled the unused text box
         }
 
         if (activeNode.responses.Length >= 1)
-            playerFirstResponse.GetComponent<TextMeshProUGUI>().text = activeNode.responses[0].ToString();
+            playerFirstResponse.GetComponent<TextMeshProUGUI>().text = activeNode.responses[0].ToString(); //Loads the player's first response into the UI 
         if (activeNode.responses.Length >= 2)
             playerSecondResponseBox.SetActive(true);
-            playerSecondResponse.GetComponent<TextMeshProUGUI>().text = activeNode.responses[1].ToString();
+            playerSecondResponse.GetComponent<TextMeshProUGUI>().text = activeNode.responses[1].ToString(); //Loads the player's second response into the UI
         if (activeNode.responses.Length >= 3)
             playerThirdResponseBox.SetActive(true);
-            playerThirdResponse.GetComponent<TextMeshProUGUI>().text = activeNode.responses[2].ToString();
+            playerThirdResponse.GetComponent<TextMeshProUGUI>().text = activeNode.responses[2].ToString(); //Loads the player's third response into the UI
         
         
     }
 
-    public void ContinueConversation()
+    public void ContinueConversation() //Is called when the player is progressing further into the dialogue tree
     {
-        int playerChoice = RecordResponse();
+        int playerChoice = RecordResponse(); //Retrieves a response from the player's input
         if(playerChoice == 0 && activeNode.repGainResponse1 != 0)
         {
-            manager.GetComponent<RelationshipManager>().UpdateCoolMeter(relationship, activeNode.repGainResponse1);
+            manager.GetComponent<RelationshipManager>().UpdateCoolMeter(relationship, activeNode.repGainResponse1); //Updates the player's rep level
         }
         if(playerChoice == 1 && activeNode.repGainResponse2 != 0)
         {
-            manager.GetComponent<RelationshipManager>().UpdateCoolMeter(relationship, activeNode.repGainResponse2);
+            manager.GetComponent<RelationshipManager>().UpdateCoolMeter(relationship, activeNode.repGainResponse2); //''
         }
         if(playerChoice == 2 && activeNode.repGainResponse3 != 0)
         {
-            manager.GetComponent<RelationshipManager>().UpdateCoolMeter(relationship, activeNode.repGainResponse3);
+            manager.GetComponent<RelationshipManager>().UpdateCoolMeter(relationship, activeNode.repGainResponse3);//''
         }
 
-        if(playerChoice >=0 && playerChoice <= 2)
+        if(playerChoice >=0 && playerChoice <= 2) //Checks if the player has made a valid choice
         {           
-                LoadNodeInfo(activeNode.children[playerChoice]);           
+                LoadNodeInfo(activeNode.children[playerChoice]); //Loads the new node chosen by the player's response        
         }
         inConvo = true;
     }
 
-    public int RecordResponse()
+    public int RecordResponse() //Is called when the player is asked for a response to dialogue
     {
         int choice = 4;
-        if(pos == 0)
+        if(pos == 0) //Checks if the player is currently selecting response 1
         {
             playerFirstResponseBox.GetComponent<Image>().color = Color.cyan;
             if(Input.GetKeyUp(KeyCode.DownArrow))
@@ -132,12 +132,12 @@ public class DialogueManager : MonoBehaviour
             }
             if(Input.GetKeyUp(KeyCode.Return))
             {
-                if (relationship >= activeNode.repLevelOption1)
+                if (relationship >= activeNode.repLevelOption1) //Checks if the player has the required rep level
                 {
                     choice = 0;
                     playerFirstResponseBox.GetComponent<Image>().color = Color.white;
                     print(choice);
-                    return choice;
+                    return choice; //Returns the player's response 
                 }
                 else if(relationship < activeNode.repLevelOption1)
                 {
@@ -145,7 +145,7 @@ public class DialogueManager : MonoBehaviour
                 }
             }
         }
-        if (pos == 1)
+        if (pos == 1) //Checks if the player is currently selecting response 2
         {
             playerSecondResponseBox.GetComponent<Image>().color = Color.cyan;
             if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -162,12 +162,12 @@ public class DialogueManager : MonoBehaviour
             
             if(Input.GetKeyUp(KeyCode.Return))
             {
-                if (relationship >= activeNode.repLevelOption2)
+                if (relationship >= activeNode.repLevelOption2) //Checks if the player has the required rep level
                 {
                     choice = 1;
                     playerSecondResponseBox.GetComponent<Image>().color = Color.white;
                     print(choice);
-                    return choice;
+                    return choice; //Returns the player's response 
                 }
                 else if (relationship < activeNode.repLevelOption2)
                 {
@@ -175,7 +175,7 @@ public class DialogueManager : MonoBehaviour
                 }
             }
         }
-        if(pos == 2)
+        if(pos == 2) //Checks if the player is currently selecting response 3
         {
             playerThirdResponseBox.GetComponent<Image>().color = Color.cyan;
             if(Input.GetKeyUp(KeyCode.UpArrow))
@@ -185,13 +185,13 @@ public class DialogueManager : MonoBehaviour
             }
             if (Input.GetKeyUp(KeyCode.Return))
             {
-                if (relationship >= activeNode.repLevelOption3)
+                if (relationship >= activeNode.repLevelOption3) //Checks if the player has the required rep level
                 {
                     choice = 2;
                     playerThirdResponseBox.GetComponent<Image>().color = Color.white;
                     print(choice);
-                    return choice;
-                 }
+                    return choice; //Returns the player's response 
+                }
             else if (relationship < activeNode.repLevelOption3)
             {
                 print("Rep Level Not High Enough");

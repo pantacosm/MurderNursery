7adc85f66ba452a7516b96eb2aa671e4e9495e55
T3Nero <ryan-16@hotmail.co.uk>
2022-12-05 19:47:53 +0000

    -RM Sentence mechanic revamp (WIP)

--- Murder Nursery/Assets/Scripts/PinBoardScripts/ReplaceRelationshipText.cs
+++ Murder Nursery/Assets/Scripts/PinBoardScripts/ReplaceRelationshipText.cs
@@ -8,100 +8,164 @@ using UnityEngine.UI;
 // attached to relationship options ui, allows player to click text to replace current relationship text
 public class ReplaceRelationshipText : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField]
-    GameObject relationshipOptionsUI;
+    //[SerializeField]
+    //GameObject relationshipOptionsUI;
 
+    OpenRelationshipOptionsPanel storyObj;
+
+    [SerializeField]
+    Transform scarletContent;
+
+    GameObject obj;
+ 
     private PinboardManager PM;
 
     private RelationshipDetails RD;
 
     string replacingText;
+    string textToReplace;
 
     private void Start()
     {
         PM = FindObjectOfType<PinboardManager>();
         RD = PM.GetComponent<RelationshipDetails>();
+        storyObj = scarletContent.GetComponent<OpenRelationshipOptionsPanel>();
     }
 
-    //// Updates undiscovered "?????" text with full conclusion 
-    void ReplaceRelationshipDetails(List<string> detailsList, List<string> relationshipList, int detailsIndex, int conclusionIndex)
-    {
-        replacingText = relationshipOptionsUI.transform.Find("RelationshipOptionsText").GetComponent<TextMeshProUGUI>().text;
+    ////// Updates undiscovered "?????" text with full conclusion 
+    //void ReplaceRelationshipDetails(List<string> detailsList, List<string> relationshipList, int detailsIndex, int conclusionIndex)
+    //{
+    //    //replacingText = relationshipOptionsUI.transform.Find("RelationshipOptionsText").GetComponent<TextMeshProUGUI>().text;
 
         
-        if (RD.textToReplace == detailsList[detailsIndex] && replacingText == RD.conclusionsList[conclusionIndex])
-        {
-            foreach (var item in relationshipList)
-            {
-                if (item == detailsList[detailsIndex])
-                {
-                    string oldText = "?????";
-                    if (detailsList[detailsIndex].Contains(oldText))
-                    {
-                        detailsList[detailsIndex] = detailsList[detailsIndex].Replace(oldText, RD.conclusionsList[conclusionIndex]);
-                        relationshipList[relationshipList.IndexOf(item)] = detailsList[detailsIndex];  //RD.conclusionsList[conclusionIndex];
-                    }
+    //    if (RD.textToReplace == detailsList[detailsIndex] && replacingText == RD.conclusionsList[conclusionIndex])
+    //    {
+    //        foreach (var item in relationshipList)
+    //        {
+    //            if (item == detailsList[detailsIndex])
+    //            {
+    //                string oldText = "?????";
+    //                if (detailsList[detailsIndex].Contains(oldText))
+    //                {
+    //                    detailsList[detailsIndex] = detailsList[detailsIndex].Replace(oldText, RD.conclusionsList[conclusionIndex]);
+    //                    relationshipList[relationshipList.IndexOf(item)] = detailsList[detailsIndex];  //RD.conclusionsList[conclusionIndex];
+    //                }
 
-                    RD.UpdateRelationship(relationshipList);
-                    //Destroy(relationshipOptionsUI);
-                    break;
-                }
-            }
-        }
-    }
+    //                RD.UpdateRelationship(relationshipList);
+    //                //Destroy(relationshipOptionsUI);
+    //                break;
+    //            }
+    //        }
+    //    }
+    //}
+
+    //void UpdateStoryText(string storyToUpdate)
+    //{
+    //    foreach (Transform item in content)
+    //    {
+    //        if(content.childCount > 0)
+    //        {
+    //            if(item.name == storyToUpdate)
+    //            {
+    //                foreach (Transform childItem in item)
+    //                {
+    //                    if(childItem.name == "One")
+    //                    {
+    //                        textToReplace = childItem.GetComponent<TextMeshProUGUI>().text;
+    //                        childItem.GetComponent<TextMeshProUGUI>().text = childItem.GetComponent<TextMeshProUGUI>().text.Replace(textToReplace, replacingText);
+    //                    }
+
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        // Goon : Juice Box
-        ReplaceRelationshipDetails(RD.goonJuiceDetails, RD.goonJuiceList, 0, 0);
-        ReplaceRelationshipDetails(RD.goonJuiceDetails, RD.goonJuiceList, 1, 1);
-        ReplaceRelationshipDetails(RD.goonJuiceDetails, RD.goonJuiceList, 2, 2);
+        replacingText = gameObject.GetComponent<TextMeshProUGUI>().text;
 
-        // Goon : Cool Guy
-        ReplaceRelationshipDetails(RD.goonCoolguyDetails, RD.goonCoolguyList, 0, 1);
-        ReplaceRelationshipDetails(RD.goonCoolguyDetails, RD.goonCoolguyList, 1, 6);
-        ReplaceRelationshipDetails(RD.goonCoolguyDetails, RD.goonCoolguyList, 2, 7);
 
-        // Goon : Dead Girl
-        ReplaceRelationshipDetails(RD.goonDeadgirlDetails, RD.goonDeadGirlList, 0, 10);
-        ReplaceRelationshipDetails(RD.goonDeadgirlDetails, RD.goonDeadGirlList, 1, 11);
-        ReplaceRelationshipDetails(RD.goonDeadgirlDetails, RD.goonDeadGirlList, 2, 12);
+        if (scarletContent.Find("One"))
+        {
+            obj = scarletContent.Find("One").gameObject;
+            if (obj == storyObj.objToReplace)
+            {
+                if (storyObj.name == "One")
+                {
+                    textToReplace = obj.GetComponent<TextMeshProUGUI>().text;
+                    obj.GetComponent<TextMeshProUGUI>().text = obj.GetComponent<TextMeshProUGUI>().text.Replace(textToReplace, replacingText);
+                }
+            }
+        }
+        if (scarletContent.Find("Two"))
+        {
+            obj = scarletContent.Find("Two").gameObject;
+            if (obj == storyObj.objToReplace)
+            {
+                if (storyObj.name == "Two")
+                {
+                    textToReplace = obj.GetComponent<TextMeshProUGUI>().text;
+                    obj.GetComponent<TextMeshProUGUI>().text = obj.GetComponent<TextMeshProUGUI>().text.Replace(textToReplace, replacingText);
+                }
+            }
+        }
 
-        // Goon : Femme
-        ReplaceRelationshipDetails(RD.goonFemmeDetails, RD.goonFemmeList, 0, 11);
-        ReplaceRelationshipDetails(RD.goonFemmeDetails, RD.goonFemmeList, 1, 1);
-        ReplaceRelationshipDetails(RD.goonFemmeDetails, RD.goonFemmeList, 2, 8);
 
-        // Juice : Cool Guy
-        ReplaceRelationshipDetails(RD.juiceCoolguyDetails, RD.juiceCoolguyList, 0, 3);
-        ReplaceRelationshipDetails(RD.juiceCoolguyDetails, RD.juiceCoolguyList, 1, 4);
-        ReplaceRelationshipDetails(RD.juiceCoolguyDetails, RD.juiceCoolguyList, 2, 5);
 
-        // Juice : Femme
-        ReplaceRelationshipDetails(RD.juiceFemmeDetails, RD.juiceFemmeList, 0, 10);
-        ReplaceRelationshipDetails(RD.juiceFemmeDetails, RD.juiceFemmeList, 1, 11);
-        ReplaceRelationshipDetails(RD.juiceFemmeDetails, RD.juiceFemmeList, 2, 8);
 
-        // Juice : Dead Girl
-        ReplaceRelationshipDetails(RD.juiceDeadgirlDetails, RD.juiceDeadGirlList, 0, 10);
-        ReplaceRelationshipDetails(RD.juiceDeadgirlDetails, RD.juiceDeadGirlList, 1, 10);
-        ReplaceRelationshipDetails(RD.juiceDeadgirlDetails, RD.juiceDeadGirlList, 2, 7);
 
-        // Cool Guy : Femme
-        ReplaceRelationshipDetails(RD.coolguyFemmeDetails, RD.coolGuyFemmeList, 0, 10);
-        ReplaceRelationshipDetails(RD.coolguyFemmeDetails, RD.coolGuyFemmeList, 1, 11);
-        ReplaceRelationshipDetails(RD.coolguyFemmeDetails, RD.coolGuyFemmeList, 2, 8);
 
-        // Cool Guy : Dead Girl
-        ReplaceRelationshipDetails(RD.coolguyDeadgirlDetails, RD.coolGuyDeadGirlList, 0, 1);
-        ReplaceRelationshipDetails(RD.coolguyDeadgirlDetails, RD.coolGuyDeadGirlList, 1, 1);
-        ReplaceRelationshipDetails(RD.coolguyDeadgirlDetails, RD.coolGuyDeadGirlList, 2, 9);
+        //// Goon : Juice Box
+        //ReplaceRelationshipDetails(RD.goonJuiceDetails, RD.goonJuiceList, 0, 0);
+        //ReplaceRelationshipDetails(RD.goonJuiceDetails, RD.goonJuiceList, 1, 1);
+        //ReplaceRelationshipDetails(RD.goonJuiceDetails, RD.goonJuiceList, 2, 2);
 
-        // Femme : Dead Girl
-        ReplaceRelationshipDetails(RD.femmeDeadgirlDetails, RD.femmeDeadGirlList, 0, 13);
-        ReplaceRelationshipDetails(RD.femmeDeadgirlDetails, RD.femmeDeadGirlList, 1, 11);
-        ReplaceRelationshipDetails(RD.femmeDeadgirlDetails, RD.femmeDeadGirlList, 2, 14);
+        //// Goon : Cool Guy
+        //ReplaceRelationshipDetails(RD.goonCoolguyDetails, RD.goonCoolguyList, 0, 1);
+        //ReplaceRelationshipDetails(RD.goonCoolguyDetails, RD.goonCoolguyList, 1, 6);
+        //ReplaceRelationshipDetails(RD.goonCoolguyDetails, RD.goonCoolguyList, 2, 7);
 
-   
+        //// Goon : Dead Girl
+        //ReplaceRelationshipDetails(RD.goonDeadgirlDetails, RD.goonDeadGirlList, 0, 10);
+        //ReplaceRelationshipDetails(RD.goonDeadgirlDetails, RD.goonDeadGirlList, 1, 11);
+        //ReplaceRelationshipDetails(RD.goonDeadgirlDetails, RD.goonDeadGirlList, 2, 12);
+
+        //// Goon : Femme
+        //ReplaceRelationshipDetails(RD.goonFemmeDetails, RD.goonFemmeList, 0, 11);
+        //ReplaceRelationshipDetails(RD.goonFemmeDetails, RD.goonFemmeList, 1, 1);
+        //ReplaceRelationshipDetails(RD.goonFemmeDetails, RD.goonFemmeList, 2, 8);
+
+        //// Juice : Cool Guy
+        //ReplaceRelationshipDetails(RD.juiceCoolguyDetails, RD.juiceCoolguyList, 0, 3);
+        //ReplaceRelationshipDetails(RD.juiceCoolguyDetails, RD.juiceCoolguyList, 1, 4);
+        //ReplaceRelationshipDetails(RD.juiceCoolguyDetails, RD.juiceCoolguyList, 2, 5);
+
+        //// Juice : Femme
+        //ReplaceRelationshipDetails(RD.juiceFemmeDetails, RD.juiceFemmeList, 0, 10);
+        //ReplaceRelationshipDetails(RD.juiceFemmeDetails, RD.juiceFemmeList, 1, 11);
+        //ReplaceRelationshipDetails(RD.juiceFemmeDetails, RD.juiceFemmeList, 2, 8);
+
+        //// Juice : Dead Girl
+        //ReplaceRelationshipDetails(RD.juiceDeadgirlDetails, RD.juiceDeadGirlList, 0, 10);
+        //ReplaceRelationshipDetails(RD.juiceDeadgirlDetails, RD.juiceDeadGirlList, 1, 10);
+        //ReplaceRelationshipDetails(RD.juiceDeadgirlDetails, RD.juiceDeadGirlList, 2, 7);
+
+        //// Cool Guy : Femme
+        //ReplaceRelationshipDetails(RD.coolguyFemmeDetails, RD.coolGuyFemmeList, 0, 10);
+        //ReplaceRelationshipDetails(RD.coolguyFemmeDetails, RD.coolGuyFemmeList, 1, 11);
+        //ReplaceRelationshipDetails(RD.coolguyFemmeDetails, RD.coolGuyFemmeList, 2, 8);
+
+        //// Cool Guy : Dead Girl
+        //ReplaceRelationshipDetails(RD.coolguyDeadgirlDetails, RD.coolGuyDeadGirlList, 0, 1);
+        //ReplaceRelationshipDetails(RD.coolguyDeadgirlDetails, RD.coolGuyDeadGirlList, 1, 1);
+        //ReplaceRelationshipDetails(RD.coolguyDeadgirlDetails, RD.coolGuyDeadGirlList, 2, 9);
+
+        //// Femme : Dead Girl
+        //ReplaceRelationshipDetails(RD.femmeDeadgirlDetails, RD.femmeDeadGirlList, 0, 13);
+        //ReplaceRelationshipDetails(RD.femmeDeadgirlDetails, RD.femmeDeadGirlList, 1, 11);
+        //ReplaceRelationshipDetails(RD.femmeDeadgirlDetails, RD.femmeDeadGirlList, 2, 14);
+
+
     }
 }
